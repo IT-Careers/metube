9097e4a19b5016b3103748da3ef3305a808d1a14
DCay <atriumandvintrigol@gmail.com>
2024-01-28 13:35:44 +0200

    MT-6 | MT-7 Implemented Registration & Login Modals

---
+++ src/Web/MeTube.Web.Models/Identity/LoginRequestModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MeTube.Web.Models.Identity;
+
+public class LoginRequestModel
+{
+    [Required]
+    [EmailAddress]
+    public string Email { get; set; }
+
+    [Required]
+    [DataType(DataType.Password)]
+    public string Password { get; set; }
+}
